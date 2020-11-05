53ccd2f15af3532bb2d51f429436db1a98d6bd56
Alapan21 <39450537+alapanc21@users.noreply.github.com>
2020-11-05 21:01:30 +0530

    Initial Commit

---
+++ BrowserTwo/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace BrowserTwo
+{
+    partial class BrowserBody
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button goButton;
+            this.backButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.textUrl = new System.Windows.Forms.TextBox();
+            this.newTabButton = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            goButton = new System.Windows.Forms.Button();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 12);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(30, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "<<";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(48, 12);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(30, 23);
+            this.forwardButton.TabIndex = 1;
+            this.forwardButton.Text = ">>";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.refreshButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.refreshButton.Location = new System.Drawing.Point(670, 12);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(56, 23);
+            this.refreshButton.TabIndex = 2;
+            this.refreshButton.Text = "refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // textUrl
+            // 
+            this.textUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textUrl.Location = new System.Drawing.Point(84, 15);
+            this.textUrl.Name = "textUrl";
+            this.textUrl.Size = new System.Drawing.Size(524, 20);
+            this.textUrl.TabIndex = 3;
+            // 
+            // goButton
+            // 
+            goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            goButton.Location = new System.Drawing.Point(614, 12);
+            goButton.Name = "goButton";
+            goButton.Size = new System.Drawing.Size(50, 23);
+            goButton.TabIndex = 5;
+            goButton.Text = "go";
+            goButton.UseVisualStyleBackColor = true;
+            goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // newTabButton
+            // 
+            this.newTabButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.newTabButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.newTabButton.Location = new System.Drawing.Point(732, 12);
+            this.newTabButton.Name = "newTabButton";
+            this.newTabButton.Size = new System.Drawing.Size(56, 23);
+            this.newTabButton.TabIndex = 2;
+            this.newTabButton.Text = "new tab";
+            this.newTabButton.UseVisualStyleBackColor = true;
+            this.newTabButton.Click += new System.EventHandler(this.newTabButton_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Location = new System.Drawing.Point(12, 41);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(776, 397);
+            this.tabControl.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 371);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // BrowserBody
+            // 
+            this.AcceptButton = goButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.refreshButton;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.newTabButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(goButton);
+            this.Controls.Add(this.textUrl);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.backButton);
+            this.Name = "BrowserBody";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Browser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BrowserBody_FormClosing);
+            this.Load += new System.EventHandler(this.BrowserBody_Load);
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.TextBox textUrl;
+        private System.Windows.Forms.Button newTabButton;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
---
+++ BrowserTwo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowserTwo
+{
+    public partial class BrowserBody : Form
+    {    
+
+        public BrowserBody()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowserBody_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            // initialize
+            Cef.Initialize(settings);
+            this.textUrl.Text = "www.google.com";
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser(this.textUrl.Text);
+            chrome.Parent = this.tabControl.SelectedTab;
+            
+            chrome.Dock = DockStyle.Fill;
+            chrome.AddressChanged += Chrome_AddressChanged;
+            chrome.TitleChanged += Chrome_TitleChanged;
+        }
+
+        private void Chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(()=>
+            {
+                this.textUrl.Text = e.Address;
+            }));
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = this.tabControl.SelectedTab.Controls[0] as ChromiumWebBrowser; 
+            if (!string.IsNullOrEmpty(this.textUrl.Text) && chrome != null) 
+                chrome.Load(this.textUrl.Text);
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = this.tabControl.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if(chrome != null) chrome.Refresh();
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = this.tabControl.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)
+            {
+                if (chrome.CanGoForward)
+                    chrome.Forward();
+            }
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = this.tabControl.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)
+            {
+                if (chrome.CanGoBack)
+                    chrome.Back();
+            }
+        }
+
+        private void BrowserBody_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void newTabButton_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "New tab";
+            tabControl.Controls.Add(tab);
+            tabControl.SelectTab(tabControl.TabCount - 1);
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser("https://www.google.com");
+            chrome.Parent = tab;
+            chrome.Dock = DockStyle.Fill;
+            this.textUrl.Text = "https://www.google.com";
+            chrome.AddressChanged += Chrome_AddressChanged;
+            chrome.TitleChanged += Chrome_TitleChanged;
+        }
+
+        private void Chrome_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                this.tabControl.SelectedTab.Text = e.Title;
+            }));
+        }
+    }
+}
